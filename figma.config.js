d865a776aab1445ee8653b9a7489c3fa6bb92875
Nikhil Iluri <nikhil.iluri@gmail.com>
2023-01-21 14:40:44 +0530

    svgs as react components

--- figma.config.js
+++ figma.config.js
@@ -1,7 +1,15 @@
 require("dotenv").config();
+const capitalize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
 const fileId = process.env.FILE_ID;
 const outputters = [
   require("@figma-export/output-components-as-svg")({ output: "./" }),
+  require("@figma-export/output-components-as-svgr")({
+    getFileExtension: () => ".tsx",
+    getComponentName: ({ componentName, pageName }) =>
+      componentName + capitalize(pageName),
+    getSvgrConfig: () => ({ typescript: true }),
+    output: "./src",
+  }),
 ];
 /** @type {import('@figma-export/types').FigmaExportRC} */
 module.exports = {
